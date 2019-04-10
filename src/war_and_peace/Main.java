18735104082f298276c1cc98f96cf5a3956d64d1
Angelina <07140714no@gmail.com>
2019-04-10 13:50:29 +0300

    Война и мир: 1 и 2 задания

--- src/war_and_peace/Main.java
+++ src/war_and_peace/Main.java
@@ -13,43 +13,26 @@ public class Main {
         File file = new File("D:\\JAVA\\Java_homework\\resources\\wp.txt");
         Scanner scanner = new Scanner(file);
         int wordCount = 0;
-        ArrayList<Integer> count = new ArrayList<>();
-        ArrayList<String> wordc = new ArrayList<>();
-        Map<Integer, ArrayList<String>> map = new HashMap<>();
+        Zadanie2 zadanie2 = new Zadanie2();
+
         while(scanner.hasNext()){
             String text = scanner.nextLine();
             String[] lines = text.toLowerCase().replaceAll("\\p{Punct}", " ").trim().split(" ");
             for (String word : lines) {
                 if(word.length() > 0){
+                    // ЗАДАНИЕ 1
                     ++wordCount;
 
-
-                    if(map.containsKey(word.length())){
-                        if(!map.get(word.length()).contains(word)){
-                            ArrayList<String> a = new ArrayList<>(map.get(word.length()));
-                            a.add(word);
-                            map.put(word.length(), a);
-                        }
-
-                    }else{
-                        ArrayList<String> list = new ArrayList<>();
-                        list.add(word);
-                        map.put(word.length(), list);
-                    }
+                    // ЗАДАНИЕ 2
+                    zadanie2.work(word);
                 }
             }
         }
+        // ЗАДАНИЕ 1
         System.out.println("Количество слов: " + wordCount);
-        for(int i = 1; i < map.size(); ++i){
-            System.out.println("Количество букв " + (i+1) + ":");
-            ArrayList<String> b = new ArrayList<>();
-            b = map.get((i+1));
-            System.out.println(b);
-            System.out.println();
-        }
+        // ЗАДАНИЕ 2
+        zadanie2.out();
 
         scanner.close();
-
     }
-
 }
